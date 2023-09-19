c8e5a415ec0381ef2381431ba12ac41f07a15645
Érico Marshall <erico.marshall@kukac.com.br>
2023-09-19 11:44:43 -0300

    [FEAT] Criada página .

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # clone-tabnews
 
 Implementação do https://www.tabnews.com.br para o https://curso.dev
+
+# Paleta de cores
+
+Black: #000000ff;
+Oxford-blue: #14213dff;
+Orange-web: #fca311ff;
+Platinum: #e5e5e5ff;
+White: #ffffffff;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,73 @@
 export default function Home() {
-  return <h1>Paulera Days, tinhamuuuuuu 😍</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#E5E5E5",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "50px",
+            fontFamily: "sans-serif",
+            color: "#FCA311",
+          }}
+        >
+          Site em construção 🚧
+        </h1>
+        <p
+          style={{
+            fontSize: "30px",
+            fontFamily: "sans-serif",
+            color: "#14213D",
+          }}
+        >
+          Acesse minhas redes:
+        </p>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            width: "300px",
+          }}
+        >
+          <a
+            href="https://www.linkedin.com/in/ericomarshall/"
+            target="blank"
+            style={{
+              fontSize: "30px",
+              fontFamily: "sans-serif",
+              color: "#14213D",
+              textDecoration: "none",
+            }}
+          >
+            Linkedin
+          </a>
+          <a
+            href="https://github.com/egmarshall"
+            target="blank"
+            style={{
+              fontSize: "30px",
+              fontFamily: "sans-serif",
+              color: "#14213D",
+              textDecoration: "none",
+            }}
+          >
+            GitHub
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
