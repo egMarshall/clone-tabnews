59b72975a38ce99f72e99127c620896c0cac4844
Érico Marshall <ericogmarshall1@gmail.com>
2024-08-07 15:24:23 -0300

    [FEAT] Adiciona dados de saúde do banco de dados à api `api/v1/status`.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,36 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  // Data Atual
+  const updatedAt = new Date().toISOString();
+
+  // Nome do Banco de Dados
+  const dbName = process.env.POSTGRES_DB;
+
+  // Versão do Banco de Dados
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  // Conexões máximas do Banco de Dados
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbMaxConnectionsResult.rows[0].max_connections;
+
+  // Conexões usadas do Banco de Dados
+  const dbOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
-    status: "ok",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: dbOpenedConnectionsValue,
+      },
+    },
   });
 }
 
